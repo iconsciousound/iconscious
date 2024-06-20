f108e1c9bb05d8aeb7a0c3ba4bf6d04b35d11dab
iconsciousound <iconsciousound@gmail.com>
2024-06-20 12:49:27 -0600

    Add files via upload

---
+++ epk.php
@@ -0,0 +1,197 @@
+<head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#000">
+
+  <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300&family=Roboto:wght@100&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Kumbh+Sans:wght@300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "MusicGroup",
+      "@id": "https://musicbrainz.org/artist/16fd1e4b-a6c6-4893-984b-d39551f9a1c2",
+      "name": "I CONSCIOUS",
+      "logo": "http://iconscious.us/black.png",
+      "image": "http://iconscious.us/images/bw.jpeg",
+      "alternateName": "I Conscious, I. CONSCIOUS, I. Conscious, i conscious, iconscious, ICONSCIOUS",
+      "url": "http://www.iconscious.us",
+      "genre": "Reggae, Reggae Rock, Reggae/Rock, Reggae Dub, Native American Reggae, Indigenous",
+      "foundingLocation": {
+        "@type": "City",
+        "name": "Zuni, NM"
+      },
+      "foundingDate": "2007",
+      "sameAs": [
+        "http://www.facebook.com/iconsciousreggae", "http://www.instagram.com/iconsciousreggae",
+        "http://www.pandora.com/i-conscious",
+        "http://www.bandsintown.com/i.conscious",
+      ]
+    }
+  </script>
+
+  <title>I CONSCIOUS EPK</title>
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="I CONSCIOUS" />
+  <meta property="og:description" content="The official website featuring news, tour dates, videos and music!" />
+  <meta property="og:image" content="https://www.iconscious.us/images/1753%202.png" />
+  <link rel="stylesheet" type="text/css" href="epk.css">
+  <link rel="stylesheet" type="text/css" href="flexslider.css">
+
+  <!-- Place somewhere in the <head> of your document -->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script src="https://www.iconscious.us/jquery.flexslider.js"></script>
+
+  <!-- Place in the <head>, after the three links -->
+  <script type="text/javascript" charset="utf-8">
+    $(window).load(function() {
+      $('.flexslider').flexslider();
+    });
+  </script>
+
+  <script id="mcjs">
+    ! function(c, h, i, m, p) {
+      m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p)
+    }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/68c37f69864ea67a88def205f/1736670ef910ff406fb81654f.js");
+  </script>
+
+  <meta name="google-site-verification" content="j6ov-QaCJpuyhsOc4znJOQVKD7d6g6-oQV671hxEVxE" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body></body>
+
+<div class="grid-container">
+  <div class="logo">
+    <img src="https://iconscious.us/images/icl3twlg.png" width="100%" height="auto">
+    Original Roots | Rock | Reggae <br>
+    Now available for booking!
+  </div>
+
+  <div class="logovid" muted loop playsinline autoplay>
+    <video muted loop playsinline autoplay>
+      <source src="https://iconscious.us/images/logovidNA0001-0250.mp4" type="video/mp4">
+      Your browser does not support HTML5 video.
+    </video>
+  </div>
+
+  <div class="about">
+    <div class="#about">
+      <h1>Bio</h1>
+      <p>
+      <div class="bio2">
+        I CONSCIOUS an original roots, rock, reggae & rnb band. Their stage show takes you on a sonic adventure influenced by classic Jamaican sound to modern day island roots music. After releasing their debut EP - the song 'Champion' was nominated for 'Best World Music Recording' at the Native American Music Awards and acknowledged at the Indigenous
+        Peoples Awards in Canada. I CONSCIOUS performed with many internationally known acts throughout their musical career, having the honor of gracing the stage with Grammy award winners. I CONSCIOUS latest full length album release
+        'Feeling Irie' is available to stream - worldwide.
+      </div>
+    </div>
+  </div>
+  <div class="music">
+    <h3>Music</h3>
+    <p><iframe src="https://open.spotify.com/embed?uri=spotify:album:0g6p4Pywk6PkqjDpchHWqd&amp;theme=white" width="100%" height="380" frameborder="0"></iframe>
+  </div>
+  <div class="vid">
+    <h3>Video</h3>
+    <div class="yt4">
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/sWqIT5ra4D8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="yt5">
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/m2_Q_ByosZM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="yt6">
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/IPXAZRp4tMY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
+  <div class="shared">
+    <h3>Shared the stage with:</h3>
+    The Original Wailers (Al Anderson)
+    <br>Black Uhuru
+    <br>Yellowman
+    <br>Maoli
+    <br>Big Mountain
+    <br>Pato Banton
+    <br>Tribal Seeds
+    <br>Hirie
+    <br>Iration
+    <br>Natural Vibrations
+    <br>Junior Reid
+    <br>New Kingston
+    <br>Warrior King
+    <br>Michigan & Smiley
+    <br>Everton Blender
+    <br>The Itals (Keith Porter)
+    <br>The Toasters
+    <br>The Meditations
+    <br> HR of Bad Brains
+    <br>
+  </div>
+
+  <div class="nat">
+    <h3>Nationwide Performances in:</h3>
+    New Mexico:
+    <br> Sandia Casino
+    <br> Camel Rock Casino
+    <br> Dancing Eagle Casino
+    <br> San Felipe Casino
+    <br> Wild Horse Casino
+    <br>KTAOS Solar Center - Taos
+    <br> St Bernard - Taos Ski Valley
+    <br>Launchpad - Albuquerque
+    <br>Moonlight Lounge
+    <br>Marble Brewery
+    <br>Burt's Tiki Lounge
+    <br>Sisters
+    <br> Boxcar - Santa Fe
+    <br> San Juan Pueblo
+    <br> Jemez Pueblo
+    <br>
+
+    <br>California:
+    <br>Patrick Malloys - Hermosa Beach
+    <br> Slidebar - Fullerton
+    <br> Stonehouse - Nevada City
+    <br> Ashkenaz - Berkeley
+    <br> Chico
+    <br>
+
+    <br> Arizona:
+    <br> Cliff Castle Casino - Camp Verde
+    <br> Last Exit Live - Phoenix
+    <br> Orpheum Theater - Flagstaff
+    <br> Tucson<br>Tempe
+    <br>Legecy Room - Tuba City
+    <br>Veterans Memorial Center- Hopi
+    <br> Hualapai<br> Supai<br> Grand Canyon
+    <br> Navajo Nation Fair - Window Rock
+    <br>
+
+    <br> Colorado: <br>Cervantes - Denver
+    <br> Ska Brewery - Durango<br> Pueblo
+    <br>
+    <br> Utah:
+    <br>Deluxe Bar -Salt Lake City<br>
+    <br> Texas:
+    <br>Ruta Maya - Austin<br>
+    <br> Florida: <br>Miami<br>
+    <br> Minnesota<br>
+    <br> Hawaii:<br>Humpy's Ale House - Kona<br> Tropics - Honolulu
+    <br>
+  </div>
+  <div class="contact">
+   Contact Info:<br>
+    <i class="fa fa-phone" style="width:30px"></i> 505-249-4781<br><i class="fa fa-envelope" style="width:30px"> </i> mgmt@iconscious.us
+  </div>
+
+  <div class="footer">
+    All Rights Reserved <br>
+    Fiyah Records 2024©
+  </div>
